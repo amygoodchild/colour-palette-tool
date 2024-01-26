5374ecc1b943b963e8270339536c0616c0edccf8
amygoodchild <amy@amygoodchild.com>
2024-01-26 16:57:54 +0000

    changes for gh pages

--- app/components/PaletteControlDrawer.js
+++ app/components/PaletteControlDrawer.js
@@ -22,11 +22,8 @@ export default function PaletteControlDrawer(){
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
           </div>
-
         </div>
-
       </div>
-
     </div>
   )
 }
